19bb8d7e8d16074627422e02006313b4d4d1273f
Daniel Walsh <walshydev@gmail.com>
2023-11-24 12:49:36 +0000

    Move scripts and update for new worker

--- worker/scripts/new_account.sh
+++ worker/scripts/new_account.sh
@@ -8,4 +8,6 @@ fi
 USERNAME="$1"
 API_TOKEN=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 64)
 
-wrangler d1 execute DB --command "INSERT INTO users (name, api_token) VALUES (\"$USERNAME\", \"$API_TOKEN\")"
+echo "API token: $API_TOKEN"
+
+wrangler d1 execute DB --env production --command "INSERT INTO users (name, api_token) VALUES (\"$USERNAME\", \"$API_TOKEN\")"
--- worker/scripts/new_project.sh
+++ worker/scripts/new_project.sh
@@ -10,13 +10,13 @@ OWNER_ID="$2"
 DESCRIPTION="A new project"
 SUPPORTED_VERSIONS="$3"
 
-wrangler d1 execute DB --command "INSERT INTO projects (user_id, name, description) VALUES ($OWNER_ID, \"$PROJECT\", \"$DESCRIPTION\")"
+wrangler d1 execute DB --env production --command "INSERT INTO projects (user_id, name, description) VALUES ($OWNER_ID, \"$PROJECT\", \"$DESCRIPTION\")"
 
-CREATE_OUTPUT=$(wrangler d1 execute DB --json --command "SELECT project_id from projects WHERE name = '$PROJECT' AND user_id = $OWNER_ID")
+CREATE_OUTPUT=$(wrangler d1 execute DB --env production --json --command "SELECT project_id from projects WHERE name = '$PROJECT' AND user_id = $OWNER_ID")
 PROJECT_ID=$(echo "$CREATE_OUTPUT" | tail -n +3 | jq .[0].results[0].project_id)
 echo "Created project $PROJECT_ID"
 
-wrangler d1 execute DB --command "INSERT INTO release_channels (
+wrangler d1 execute DB --env production --command "INSERT INTO release_channels (
 	project_id,
 	name,
 	supported_versions,
