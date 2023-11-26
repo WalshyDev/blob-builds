2b204e35a227874f8d239ecf07d6ad96dda64a8d
Daniel Walsh <walshydev@gmail.com>
2023-11-26 18:50:10 +0000

    Update docs + UI

--- worker/src/handlers/projects/project.ts
+++ worker/src/handlers/projects/project.ts
@@ -26,15 +26,15 @@ export async function getProject(ctx: Context) {
 export const newProjectSchema = z.object({
 	name: z.string().min(3).max(64),
 	description: z.string().min(6).max(2000).optional(),
-	release_channels: z.array(z.object({
+	releaseChannels: z.array(z.object({
 		name: z.string(),
-		supported_versions: z.string(),
+		supportedVersions: z.string(),
 		dependencies: z.array(z.string()).default([]),
-		file_naming: z.string().default('$project.jar'),
+		fileNaming: z.string().default('$project.jar'),
 	})).default([
 		{
 			name: 'Dev',
-			supported_versions: 'Unknown',
+			supportedVersions: 'Unknown',
 		},
 	]),
 });
@@ -62,12 +62,12 @@ export async function postNewProject(ctx: Ctx, body: Body) {
 	}
 
 	// Create release channels
-	const channels: InsertReleaseChannel[] = body.release_channels.map(channel => ({
+	const channels: InsertReleaseChannel[] = body.releaseChannels.map(channel => ({
 		projectId: project.projectId,
 		name: channel.name,
-		supportedVersions: channel.supported_versions,
+		supportedVersions: channel.supportedVersions,
 		dependencies: channel.dependencies,
-		fileNaming: channel.file_naming,
+		fileNaming: channel.fileNaming,
 	}));
 
 	await ReleaseChannelStore.insertNewReleaseChannel(channels);
