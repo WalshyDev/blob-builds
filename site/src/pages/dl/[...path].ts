d8b130c5681c78ffcb67cda0af635c0297479d6e
Daniel Walsh <walshydev@gmail.com>
2024-01-24 11:18:24 +0000

    Fix dl

--- site/src/pages/dl/[...path].ts
+++ site/src/pages/dl/[...path].ts
@@ -1,13 +1,16 @@
 import type { APIContext } from 'astro';
 
-export async function GET({ request }: APIContext) {
+export async function GET({ request, locals }: APIContext) {
 	try {
-		console.log('Requesting dl');
-
 		const url = new URL(request.url);
 
-		// Prefix with /api
-		url.pathname = `/api${url.pathname}`;
+		// Point to API
+		// TODO: I should either enforce API_URL or move this to a const
+		// (ideally not even need to point to workers.dev but shit is broke)
+		url.hostname = locals.runtime.env.API_URL.replace('https://', '')
+			?? 'blob-builds-api-production.walshydev.workers.dev';
+
+		console.log('Requesting dl - fetching ' + url.toString());
 
 		return fetch(url, request);
 	} catch(e) {
