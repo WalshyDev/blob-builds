f87251246eb5520c6fc0c3bc1b7f2830fb502b94
Daniel Walsh <walshydev@gmail.com>
2024-03-02 13:19:09 +0000

    Make release notes render as markdown

--- site/src/components/projects/builds/BuildCard.tsx
+++ site/src/components/projects/builds/BuildCard.tsx
@@ -1,7 +1,8 @@
+import Markdown from 'react-markdown';
 import ButtonLink from '~/components/html/ButtonLink';
 import { H3 } from '~/components/html/Headings';
 import HighlightedSpan from '~/components/html/HighlightedSpan';
-import Link from '~/components/html/Link';
+import Link, { ExternalLink } from '~/components/html/Link';
 
 interface Props {
 	project: ProjectResponse;
@@ -22,8 +23,8 @@ export default function BuildCard({ build }: Props) {
 					<Link href={build.commitLink}>{build.commitHash.substring(0, 8)}</Link>
 				)}
 				{build.commitHash && build.commitLink && build.releaseNotes && ' '}
-				{/* {renderReleaseNotes(project, build)} */}
-				{build.releaseNotes}
+
+				{build.releaseNotes && <ReleaseNotes releaseNotes={build.releaseNotes} />}
 			</p>
 
 			<div className='my-2'>
@@ -37,4 +38,44 @@ export default function BuildCard({ build }: Props) {
 			</div>
 		</div>
 	);
+}
+
+// log props to see what we can use
+function ReleaseNotes({ releaseNotes }: { releaseNotes: string }) {
+	return <Markdown
+		allowedElements={['ul', 'ol', 'li', 'a', 'h1', 'h2', 'h3', 'h4', 'p', 'span', 'div']}
+		skipHtml={true}
+		components={{
+			// Style lists
+			ul({ children }) {
+				return <div className='list-disc'>{children}</div>;
+			},
+			ol({ children }) {
+				return <div className='list-decimal'>{children}</div>;
+			},
+			// Make all links marked as external
+			a({ href, children }) {
+				return <ExternalLink href={href ?? '#'}>{children}</ExternalLink>;
+			},
+			// We won't render headers, instead just make them bold
+			h1({ children }) {
+				return <div className='font-bold'>{children}</div>;
+			},
+			h2({ children }) {
+				return <div className='font-bold'>{children}</div>;
+			},
+			h3({ children }) {
+				return <div className='font-bold'>{children}</div>;
+			},
+			h4({ children }) {
+				return <div className='font-bold'>{children}</div>;
+			},
+			// We disallow html but for extra measure kill script
+			script() {
+				return null;
+			},
+		}}
+	>
+		{releaseNotes}
+	</Markdown>;
 }
